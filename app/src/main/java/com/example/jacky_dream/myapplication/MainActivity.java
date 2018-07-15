1904068abec33973e8e3063528a1a5289e1c987a
jacky-dream <237492628@qq.com>
2018-07-15 20:45:42 +0800

    哈哈哈

--- app/src/main/java/com/example/jacky_dream/myapplication/MainActivity.java
+++ app/src/main/java/com/example/jacky_dream/myapplication/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //S这是我的一个测试
     }
 }
